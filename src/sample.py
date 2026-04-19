14245d816940a870b9074ce4a33347c821ed6df5
David Cooper <11625114+dc9701@users.noreply.github.com>
2026-04-18 20:06:52 -0700

    First passing EX-VAL

--- src/sample.py
+++ src/sample.py
@@ -1,11 +1,8 @@
 import great_expectations as gx
-from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
 import os
-# DCC from ruamel import yaml
 
 # Define and print data context.
 context = gx.get_context(mode="file", project_root_dir="./resources")
-# DCC context = gx.get_context()
 
 # Define Snowflake connection.
 acct_name = os.getenv("MY_DB_ACCOUNT")
@@ -27,8 +24,6 @@ datasource = context.data_sources.add_or_update_snowflake(
     connection_string=sf_conn_string,
 )
 
-# DCC print("[DCC_DEBUG_01] CONTEXT W/ DATASOURCE:\n", context)
-
 # Add CLAIMS table asset and define Batch Request
 table_asset = datasource.add_table_asset(
     name="claims_asset",
@@ -40,5 +35,12 @@ batch_request = table_asset.build_batch_request()
 validator = context.get_validator(
     batch_request=batch_request
 )
-print(validator.head())
+# DCC print("[DCC_DEBUG_01] VALIDATOR:\n", validator.head())
 
+result_01 = validator.expect_column_values_to_be_in_set(
+    column="EMPLOYER_NAME",
+    value_set=["Redacted"],
+    mostly=.1425
+).success
+
+print(f"[DCC_DEBUG_01] EXPECT EMPLOYER_NAME equals 'Redacted' about 15% of the time: {'TRUE' if result_01 else 'FALSE'}\n")
