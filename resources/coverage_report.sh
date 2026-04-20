e258dd41bc8057b9991e50acbf4845c00d722fd0
David Cooper <11625114+dc9701@users.noreply.github.com>
2026-04-20 14:01:22 -0700

    Delete some unneeded files

--- resources/coverage_report.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pytest --cov=. --cov-branch --cov-report html --cov-config=.coveragerc -c ./pytest.ini
-pytest --cov=. --cov-branch --cov-report xml --cov-config=.coveragerc -c ./pytest.ini
